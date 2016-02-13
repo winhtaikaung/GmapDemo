181a63a32a0f543f08742ddc008f0849d87e54dd
ဝင်းထိုက်အောင် <winhtaikaung28@hotmail.com>
2016-02-13 12:12:23 +0800

    Initial Commit

---
+++ app/src/main/java/com/googlemapdemo/MainActivity.java
@@ -0,0 +1,17 @@
+package com.googlemapdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FrameLayout container=findViewById(R.id.container);
+
+    }
+}
